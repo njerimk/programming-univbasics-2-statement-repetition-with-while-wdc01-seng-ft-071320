463eba901ab63cb3dbd784df5bfad3140c3df6ee
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-27 15:24:37 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,11 @@
 # Write your code here
 count = 10
 exit_number= 1
+
 while count > 10 && count == exit_number
   puts count
   count = count -= 1
+if count == exit number
+  puts "Happy New Year!"
 end
+
